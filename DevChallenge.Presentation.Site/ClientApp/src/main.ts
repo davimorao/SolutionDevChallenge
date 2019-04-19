8e117dcb1ab10ea1fe809bf571e4f905a6aa84c9
Davi Morão <davimorao@gmail.com>
2019-04-19 12:38:29 -0300

    Url Api Local e Produção no Angular

--- DevChallenge.Presentation.Site/ClientApp/src/main.ts
+++ DevChallenge.Presentation.Site/ClientApp/src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+var apiUrl = {
+  base_dev: "https://localhost:44324/api/",
+  base_prod: "http://api-devchallenge.azurewebsites.net/api/"
+}
+
 export function getBaseUrl() {
-  return "http://api-devchallenge.azurewebsites.net/api/";
-  // return "https://localhost:44324/api/";
+  if (environment.production) {
+    return apiUrl.base_prod;
+  } else {
+    return apiUrl.base_dev;
+  }
 }
 
 export const providers = [
